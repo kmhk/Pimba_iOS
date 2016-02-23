5fed405a829a48ee5360d319bd2ce0ea1e8db1fb
herocules <herocules@heroculess-mac.local>
2016-02-24 02:12:57 +0800

    Initial Commit

---
+++ PIMBA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PIMBA
+//
+//  Created by herocules on 2/24/16.
+//  Copyright © 2016 herocules. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
